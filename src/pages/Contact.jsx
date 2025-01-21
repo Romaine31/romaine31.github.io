f67d11f17a04d49b16b9898b9f2f24ec499d2d1d
RomaineDoesStuff <rcagubcub2@gmail.com>
2025-01-21 11:38:12 +0800

    fixed contact images

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom"
+import gitImg from "/src/assets/github.png?url"
+import linkedinImg from "/src/assets/linkedin.png?url"
 
 export default function About(){
     return(
         <div className="myContact">
             <p>Email me at:<br/><u>romaine.cagubcub@gmail.com</u><br/>
             <Link to="https://github.com/Romaine31" target="_blank">
-            <img src={import.meta.env.BASE_URL + "src/assets/github.png"} style={{height: "30px", width: "30px"}}/>
+            <img src={gitImg} style={{height: "30px", width: "30px"}}/>
             </Link>
             <Link to="https://linkedin.com/in/imromaine" target="_blank">
-            <img src={ import.meta.env.BASE_URL + "src/assets/linkedin.png"} style={{height: "30px", width: "30px"}}/>
+            <img src={linkedinImg} style={{height: "30px", width: "30px"}}/>
             </Link>
             </p>
         </div>
